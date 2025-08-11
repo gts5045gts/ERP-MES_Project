b20425fe23d44df1baeb42d142bf177662badd84
rladntjd85 <rladntjd850@gmail.com>
2025-08-12 07:01:55 +0900

    경로 수정

--- bootstrap_stater/src/main/java/com/bootstrap/study/e_approval/controller/EappController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/e_approval/controller/EappController.java
@@ -1,19 +1,20 @@
 package com.bootstrap.study.e_approval.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/e_app")
+@RequestMapping("/e_approval")
+@Log4j2
 public class EappController {
 
     @GetMapping("/doc_list")
     public String doc_list(Model model){
-
-
-        return "/e_app/doc_list";
+        return "/e_approval/e_app_doc_list";
     }
 
 
