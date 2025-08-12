fdfba42d1f60e8de915e54b8f735a753160aa39b
ITWILL <ITWILL@ITWILL4-6>
2025-08-12 12:41:18 +0900

    참고페이지 없앰

--- bootstrap_stater/src/main/java/com/bootstrap/study/controller/WebController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/controller/WebController.java
@@ -28,55 +28,5 @@ public class WebController {
 
         return "blank";
     }
-	@RequestMapping("404")
-	public String a404(){
-		
-		return "/example/404";
-	}
-	@RequestMapping("buttons")
-	public String buttons(){
-		
-		return "/example/buttons";
-	}
-	@RequestMapping("cards")
-	public String cards(){
-		
-		return "/example/cards";
-	}
-	@RequestMapping("charts")
-	public String charts(){
-		
-		return "/example/charts";
-	}
-	@RequestMapping("index2")
-	public String index2(){
-		
-		return "/example/index2";
-	}
-	@RequestMapping("tables")
-	public String tables(){
-		
-		return "/example/tables";
-	}
-	@RequestMapping("utill_animation")
-	public String utill_animation(){
-		
-		return "/example/utill_animation";
-	}
-	@RequestMapping("utill_border")
-	public String utill_border(){
-		
-		return "/example/utill_border";
-	}
-	@RequestMapping("utill_color")
-	public String utill_color(){
-		
-		return "/example/utill_color";
-	}
-	@RequestMapping("utill_other")
-	public String utill_other(){
-		
-		return "/example/utill_other";
-	}
 
 }
