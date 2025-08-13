f4049b880994c1a6610bd3f7cdf7f1fad7517f35
woosung kim <rladntjd850@gmail.com>
2025-08-13 09:10:02 +0900

    app controller 경로 수정

--- bootstrap_stater/src/main/java/com/bootstrap/study/approval/controller/apprController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/approval/controller/apprController.java
@@ -2,7 +2,6 @@ package com.bootstrap.study.approval.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
