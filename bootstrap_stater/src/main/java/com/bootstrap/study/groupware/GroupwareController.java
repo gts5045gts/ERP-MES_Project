45549c2dca8a57122c710e9a0ba8d1c3e9d11942
ITWILL <ITWILL@ITWILL4-9>
2025-08-13 13:55:11 +0900

    groupware 페이지작성중

--- bootstrap_stater/src/main/java/com/bootstrap/study/groupware/GroupwareController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/groupware/GroupwareController.java
@@ -1,5 +1,31 @@
 package com.bootstrap.study.groupware;
 
-public class GroupwareController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bootstrap.study.personnel.controller.PersonnelController;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/groupware")
+@Log4j2
+public class GroupwareController {
+	
+	@GetMapping("/gwMain")
+	public String gwMain() {
+		log.info("PersonnelController orgChart()");
+		
+		return "/gw/gwMain";
+	}
+	@GetMapping("/aNotice")
+	public String regist() {
+		log.info("PersonnelController regist()");
+		
+		return "/gw/aNotice";
+	}
+	
 }
