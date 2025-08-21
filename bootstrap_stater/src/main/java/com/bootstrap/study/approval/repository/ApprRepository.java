51b6e83cfe59e91f4df734082f8fda7d5fca1e5d
naealrado2 <naealrado2@naver.com>
2025-08-21 15:33:50 +0900

    전자결재 승인,반려

--- bootstrap_stater/src/main/java/com/bootstrap/study/approval/repository/ApprRepository.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/approval/repository/ApprRepository.java
@@ -2,10 +2,14 @@ package com.bootstrap.study.approval.repository;
 
 import com.bootstrap.study.approval.entity.Appr;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -25,10 +29,32 @@ public interface ApprRepository extends JpaRepository<Appr,Long> {
 		        a.req_type,
 		        a.emp_id,
 		        a.current_step
-		    FROM C##TEAM1.approval_line al
-		    JOIN C##TEAM1.approval a ON al.req_id = a.req_id
-		    JOIN C##TEAM1.employee e ON a.emp_id = e.emp_id
+		    FROM approval_line al
+		    JOIN approval a ON al.req_id = a.req_id
+		    JOIN employee e ON a.emp_id = e.emp_id
 		    ORDER BY a.create_at DESC, al.step_no ASC
 		    """, nativeQuery = true)
     List<Object[]> findApprovalListWithJoin();
+    
+    
+    //0821
+   	// 승인버튼 누를시 승인처리되게 하기 (결재목록에서 대기 -> 승인으로 바뀜, 데이터도 반영)
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = """
+        UPDATE approval_line 
+        SET decision = :decision, 
+            dec_date = SYSDATE,
+            comments = :comments
+        WHERE req_id = :reqId 
+        AND decision = 'PENDING'
+        AND step_no = (
+            SELECT MIN(step_no) 
+            FROM approval_line 
+            WHERE req_id = :reqId 
+            AND decision = 'PENDING'
+        )
+        """, nativeQuery = true)
+    int updateApprovalLineDecision(@Param("reqId") Long reqId, 
+                                  @Param("decision") String decision,
+                                  @Param("comments") String comments);
 }
