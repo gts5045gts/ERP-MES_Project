1f498eb3da965060ce05ff8c492e6003c2a06951
ITWILL <ITWILL@ITWILL4-9>
2025-08-22 17:22:22 +0900

    spring security 로그인 완료

--- bootstrap_stater/src/main/java/com/bootstrap/study/personnel/dto/PersonnelLoginDTO.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/personnel/dto/PersonnelLoginDTO.java
@@ -29,13 +29,37 @@ public class PersonnelLoginDTO implements UserDetails {
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.passwd;
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.empId;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
