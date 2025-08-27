f4d2fcae8cf27d156697f29b5d27501caba37d7d
woosung kim <rladntjd850@gmail.com>
2025-08-27 17:36:15 +0900

    공통 문서 양식 진행중

--- bootstrap_stater/src/main/java/com/bootstrap/study/groupware/controller/GroupwareController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/groupware/controller/GroupwareController.java
@@ -1,10 +1,20 @@
 package com.bootstrap.study.groupware.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bootstrap.study.commonCode.dto.CommonCodeDTO;
+import com.bootstrap.study.commonCode.dto.CommonDetailCodeDTO;
+import com.bootstrap.study.commonCode.entity.CommonDetailCode;
+import com.bootstrap.study.commonCode.service.CommonCodeService;
+import com.bootstrap.study.groupware.dto.DocumentDTO;
+import com.bootstrap.study.groupware.dto.NoticeDTO;
 import com.bootstrap.study.personnel.controller.PersonnelController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +23,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/groupware")
 @Log4j2
+@RequiredArgsConstructor
 public class GroupwareController {
+	
+	private final CommonCodeService comService;
 	
 //	@GetMapping("/notice")
 //	public String notice() {
@@ -37,8 +50,10 @@ public class GroupwareController {
 	}
 	
 	@GetMapping("/docWrite")
-	public String docWrite() {
-		log.info("GroupwareController docWrite()");
+	public String docWrite(Model model) {
+//		log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+codeDetail);
+		model.addAttribute("dtCodes", comService.findByComId("DOC"));
+		model.addAttribute("documentDTO", new DocumentDTO());
 		
 		return "/gw/docWrite";
 	}
--- bootstrap_stater/src/main/java/com/bootstrap/study/groupware/dto/DocumentDTO.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/groupware/dto/DocumentDTO.java
@@ -1,5 +1,44 @@
 package com.bootstrap.study.groupware.dto;
 
-public class DocumentDTO {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
+import com.bootstrap.study.approval.constant.ApprStatus;
+import com.bootstrap.study.approval.dto.ApprDTO;
+import com.bootstrap.study.approval.dto.ApprDetailDTO;
+import com.bootstrap.study.approval.dto.ApprLineDTO;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class DocumentDTO {
+	
+	private Long docId;
+    private String empId;
+    private String docTitle;
+    private String docContent;
+    private String docType;
+    private Date createAt;
+    private Date updateAt;
+    
+    @Builder
+	public DocumentDTO(Long docId, String empId, String docTitle, String docContent, String docType, Date createAt,
+			Date updateAt) {
+		this.docId = docId;
+		this.empId = empId;
+		this.docTitle = docTitle;
+		this.docContent = docContent;
+		this.docType = docType;
+		this.createAt = createAt;
+		this.updateAt = updateAt;
+	}
 }
