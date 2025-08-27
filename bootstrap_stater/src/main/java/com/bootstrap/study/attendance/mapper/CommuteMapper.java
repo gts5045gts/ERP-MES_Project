f2a1a7f2b7b3d5deb69b94fbeaab20b97eddefb0
minseok <alstjr67532@gmail.com>
2025-08-27 18:04:48 +0900

    2025.08.27 - CMS

--- bootstrap_stater/src/main/java/com/bootstrap/study/attendance/mapper/CommuteMapper.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/attendance/mapper/CommuteMapper.java
@@ -30,9 +30,6 @@ public interface CommuteMapper {
 	// 퇴근버튼 눌렀을때 퇴근시간 commute_record에 저장
 	void updateCommuteCheckOut(CommuteDTO commute);
 
-	// 관리자 근태관리 리스트
-//	List<CommuteDTO> getAdminDeptCommuteList(Map<String, Object> paramMap);
-
 	// 부서 공통코드
 	List<CommonDetailCodeDTO> getCommonDept(String comId);
 
