647d338fe5c8ce42cedcbd0dd8d789b48f9fb632
ITWILL <whdtjr21c@naver.com>
2025-09-04 09:27:54 +0900

    그룹웨어 수정250904 noticeController 수정

--- bootstrap_stater/src/main/java/com/bootstrap/study/groupware/controller/NoticeController.java
+++ bootstrap_stater/src/main/java/com/bootstrap/study/groupware/controller/NoticeController.java
@@ -34,14 +34,19 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class NoticeController {
 
-	@Autowired
-	private NoticeRepository noticeRepository;
-	@Autowired
-	private NoticeService noticeService;
-	@Autowired
-	private PersonnelRepository personnelRepository;
-	@Autowired
-	private CommonCodeService commonCodeService;
+	private final NoticeRepository noticeRepository;
+	private final NoticeService noticeService;
+	private final PersonnelRepository personnelRepository;
+	private final CommonCodeService commonCodeService;
+
+	public NoticeController(NoticeRepository noticeRepository, NoticeService noticeService,
+			PersonnelRepository personnelRepository, CommonCodeService commonCodeService) {
+		super();
+		this.noticeRepository = noticeRepository;
+		this.noticeService = noticeService;
+		this.personnelRepository = personnelRepository;
+		this.commonCodeService = commonCodeService;
+	}
 
 	// 공지사항 목록 페이지를 보여주는 메서드
 	@GetMapping("")
