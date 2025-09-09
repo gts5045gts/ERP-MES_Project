28b03e74f6fab7ae2bb09e69ba61c8b45a2a126b
ITWILL <ITWILL@ITWILL4-6>
2025-09-09 16:43:26 +0900

    com.erp_mes.mes 패키지 인식 할수 있게 설정

--- erp_mes/src/main/java/com/erp_mes/erp/ErpMesApplication.java
+++ erp_mes/src/main/java/com/erp_mes/erp/ErpMesApplication.java
@@ -3,10 +3,12 @@ package com.erp_mes.erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"com.erp_mes.erp", "com.erp_mes.mes"})
 public class ErpMesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
--- erp_mes/src/main/java/com/erp_mes/mes/plant/controller/ProcessController.java
+++ erp_mes/src/main/java/com/erp_mes/mes/plant/controller/ProcessController.java
@@ -1,5 +1,24 @@
 package com.erp_mes.mes.plant.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/plant")
+@RequiredArgsConstructor
+@Log4j2
 public class ProcessController {
 
+	
+	@GetMapping("/process")
+	public String process() {
+		log.info("완성");
+		
+		
+		return "/plant/process";
+	}
 }
