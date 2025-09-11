180af899b58049e78a9b07e20ac0c489bb14f5c0
rladntjd85 <rladntjd850@gmail.com>
2025-09-11 17:46:34 +0900

    lotrepository 안됨

--- erp_mes/src/main/java/com/erp_mes/erp/ErpMesApplication.java
+++ erp_mes/src/main/java/com/erp_mes/erp/ErpMesApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"com.erp_mes.erp", "com.erp_mes.mes"})
+//@EnableJpaRepositories(basePackages = {"com.erp_mes"}) // com.erp_mes 하위 모든 Repository 스캔
 public class ErpMesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
---
+++ erp_mes/src/main/java/com/erp_mes/mes/lot/entity/LotMaster.java
@@ -0,0 +1,49 @@
+package com.erp_mes.mes.lot.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "lot_master")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class LotMaster {
+ 	@Id
+ 	@Column(updatable = false, length = 50)
+    private String lotId;
+
+//RM, PR, FG, QA 등
+ 	@Column(length = 20, nullable = false)
+    private String type; 
+ 	
+    @Column(length = 50)
+    private String materialCode;
+    
+//수량
+    private int qty;
+    
+    @Column(length = 50)
+    private String machineId;
+    
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    
+    
+    
+}
