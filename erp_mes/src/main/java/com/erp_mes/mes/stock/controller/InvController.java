46842b8b0bd47d83f4e518bbf4ada731e6c9e6b1
naealrado2 <naealrado2@naver.com>
2025-09-12 17:20:57 +0900

    입고 화면 구성

--- erp_mes/src/main/java/com/erp_mes/mes/stock/controller/InvController.java
+++ erp_mes/src/main/java/com/erp_mes/mes/stock/controller/InvController.java
@@ -11,37 +11,43 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;  
 @Log4j2  
 @Controller
-@RequestMapping("/inventory")
 @RequiredArgsConstructor
 public class InvController {
     
 	private final InvService invService;
 	
 	// 재고 현황
-	@GetMapping("/stock")
+	@GetMapping("/inventory/stock")
 	public String stockList(Model model) {
 	    log.info("재고 현황 페이지 접속");
 	    return "inventory/stock_list";
 	}
 	
+	// 입고 관리
+	@GetMapping("/purchase/goods")
+	public String inboundList(Model model) {
+	    log.info("입고 관리 페이지 접속");
+	    return "inventory/inbound_list";
+	}
+	
 	// 출고 관리
-	@GetMapping("/outbound")
+	@GetMapping("/inventory/outbound")
 	public String outboundList(Model model) {
 	    log.info("출고 관리 페이지 접속");
 	    return "inventory/outbound_list";
 	}
 	
 	// 기준정보관리 - 자재정보(소재)
-	@GetMapping("/material")
+	@GetMapping("/inventory/material")
 	public String materialList(Model model) {
 	    log.info("출고 관리 페이지 접속");
 	    return "inventory/material_list";
 	}
 	
 	// 기준정보관리 - 제품정보(완제품/반제품)
-		@GetMapping("/item")
-		public String itemList(Model model) {
-		    log.info("출고 관리 페이지 접속");
-		    return "inventory/item_list";
-		}
+	@GetMapping("/inventory/item")
+	public String itemList(Model model) {
+	    log.info("출고 관리 페이지 접속");
+	    return "inventory/item_list";
+	}
 }
